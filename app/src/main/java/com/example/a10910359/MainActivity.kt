d4123a68ba4ec963aff7fa87a518b6d2ff7e4de3
user <teddy901217@gmail.com>
2022-03-30 14:09:51 +0800

    show hello!!
    顯示hello的功能已經完成

--- app/src/main/java/com/example/a10910359/MainActivity.kt
+++ app/src/main/java/com/example/a10910359/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a10910359
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.example.a10910359.databinding.ActivityMainBinding
 
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         var mytext:TextView=findViewById(R.id.t1)
         mytext.text="hello"
 
-
+        var btn1=findViewById<Button>(R.id.button1)
 
     }
 
