50924ea08ce89363807bf95c5c3dc864d421ec63
user <teddy901217@gmail.com>
2022-03-30 14:07:58 +0800

    show hello!!
    顯示hello的功能已經完成

---
+++ app/src/main/java/com/example/a10910359/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.a10910359
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.a10910359.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var mytext:TextView=findViewById(R.id.t1)
+        mytext.text="hello"
+
+
+
+    }
+
+
+}
